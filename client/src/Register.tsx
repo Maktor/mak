e27fe2b72c2dc25db965d8837dfff971311425f8
Mahdiyar Bahreman <66954908+Maktor@users.noreply.github.com>
2023-07-21 19:24:47 -0400

    Update Register.tsx

--- client/src/Register.tsx
+++ client/src/Register.tsx
@@ -39,7 +39,7 @@ const Register = () => {
     } else {
       try {
         //const response = await fetch("http://localhost:3000/register", {method: "POST",headers: {"Content-Type": "application/json"},body: JSON.stringify({ firstName, username, age, email, password })});
-        const response = await fetch("https://mak-self-development-4ifpjeej2-maktor.vercel.app/register", {method: "POST",headers: {"Content-Type": "application/json"},body: JSON.stringify({ firstName, username, age, email, password })});
+        const response = await fetch("https://mak-self-development-4ifpjeej2-maktor.vercel.app/api/register", {method: "POST", mode: "cors", headers: { "Content-Type": "application/json" },body: JSON.stringify({ firstName, username, age, email, password })});
         if (response.ok) {
           console.log("User registered successfully");
           localStorage.setItem("isLoggedIn", "true");
@@ -62,7 +62,7 @@ const Register = () => {
 
     try {
       //const response = await fetch("http://localhost:3000/login", {method: "POST",headers: {"Content-Type": "application/json"},body: JSON.stringify({ username, password })});
-      const response = await fetch("https://mak-self-development-4ifpjeej2-maktor.vercel.app/login", {method: "POST",headers: {"Content-Type": "application/json"},body: JSON.stringify({ username, password })});
+      const response = await fetch("https://mak-self-development-4ifpjeej2-maktor.vercel.app/api/login", {method: "POST", mode: "cors", headers: { "Content-Type": "application/json" },body: JSON.stringify({ username, password })});
 
       if (response.ok) {
         const data = await response.json();
